5975e3586381a3d251d58aa00599e919b5da577b
MathieuPruvot <44382696+MathieuPruvot@users.noreply.github.com>
2018-10-26 09:43:41 +0100

    Update vue.php

--- vue.php
+++ vue.php
@@ -1,1 +1,19 @@
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Traitement inscription</title>
+        <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <h1>Inscription</h1>
+      
+          <!-- formulaire -->
+
+        <h1>Inscrits</h1>
+  
+          <!-- tableau -->
+    </body>
+</html>
