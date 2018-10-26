38dc21269a4f11f197b3f4ba91d4de96d745bd14
MathieuPruvot <44382696+MathieuPruvot@users.noreply.github.com>
2018-10-26 09:46:02 +0100

    Update modele.php

--- modele.php
+++ modele.php
@@ -1,1 +1,30 @@
 
+<?php
+function ()
+{
+
+}
+
+function )
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+    $sql=''//requete sql
+    $req = $db->prepare($sql);
+    $req->execute(array($postId));
+    $result = $req->fetchall();
+
+    return;
+}
+
+function ()
+{
+   
+    return $comments;
+}
