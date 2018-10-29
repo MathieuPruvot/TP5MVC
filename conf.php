897cc44c5bae8b6bb9fa538e31f70962cf5f9f64
MathieuPruvot <44382696+MathieuPruvot@users.noreply.github.com>
2018-10-29 12:12:13 +0100

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,4 +1,6 @@
-		define("host", "localhost");
-		define("dbname", "Inscription");
-		define("user", "root");
-		define("pass", "root");
+<?php
+	define("USER", "root"); // constante nom d'utilisateur
+	define("PASS", "root"); // constante mot de passe
+	define("HOST", "localhost"); // constante adresse BDD
+	define("DBNAME", "Inscriptions"); // constante nom de la base
+?>
