6b7cc80e01a98a90d8d42fbcf3f04c82e6bc7845
MathieuPruvot <44382696+MathieuPruvot@users.noreply.github.com>
2018-10-31 11:55:23 +0100

    Create autoload.php

---
+++ TP5/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function myAutoLoad($classname){
+
+	$appDir =__DIR__ . DIRECTORY_SEPARATOR;
+	$codeDirs = array('Modele',);
+	
+	foreach($codeDirs as $dir){
+		$filepath = $appDir.$dir. DIRECTORY_SEPARATOR .$classname.'.php';
+		if(is_file($filepath)){
+			require_once $filepath;
+		}
+	}
+}
+spl_autoload_register('myAutoLoad');
+?>
