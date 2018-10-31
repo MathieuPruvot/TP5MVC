e08dbd81508542a81895325e4fe25473ef2a049a
MathieuPruvot <44382696+MathieuPruvot@users.noreply.github.com>
2018-10-31 11:58:02 +0100

    Delete conf.php

--- conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("USER", "root"); // constante nom d'utilisateur
-	define("PASS", "root"); // constante mot de passe
-	define("HOST", "localhost"); // constante adresse BDD
-	define("DBNAME", "Inscriptions"); // constante nom de la base
-?>
